872b6e22cf3b2df537f92e23a1e762baf0ca6aab
Nishant lamba <131985675+nishantlamba11@users.noreply.github.com>
2025-02-20 09:54:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function insert_Row() {
     //Write your code here
-  
+  let btn=document.querySelector("button");
+  let table=document.querySelector("#sampleTable");
+ 
+	let newRow=table.insertRow(0);
+	let cell1=newRow.insertCell(0);
+	let cell2=newRow.insertCell(1);
+	cell1.innerText="New Cell1"
+	cell2.innerText="New Cell2"
+   
   
 }
